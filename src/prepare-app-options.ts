ec34bfd531d7426c94ef23cc5e2d5983b50e7037
rangelak <rgmilushev@gmail.com>
2022-07-01 22:37:40 +0200

    Getting the CLI to work with URLs

--- src/prepare-app-options.ts
+++ src/prepare-app-options.ts
@@ -4,64 +4,67 @@ import { parseOutputPathAndFormat } from "./parse-output-path-and-format";
 import { colors } from "./colors";
 
 interface Argv {
-  input: string;
-  output: string;
-  image_format: string;
-  image_quality: number;
-  timeout: number;
-  width: number;
-  height: number;
-  color?: string;
-  model_viewer_version?: string;
-  model_viewer_attributes?: string;
+	input: string;
+	output: string;
+	image_format: string;
+	image_quality: number;
+	timeout: number;
+	width: number;
+	height: number;
+	color?: string;
+	model_viewer_version?: string;
+	model_viewer_attributes?: string;
 }
 
 interface Props {
-  modelPort: number;
-  argv: Argv;
-  debug?: boolean;
+	modelPort: number;
+	argv: Argv;
+	debug?: boolean;
 }
 
 export function prepareAppOptions({ modelPort, debug, argv }: Props) {
-  const {
-    input,
-    output,
-    image_format,
-    image_quality: quality,
-    timeout,
-    height,
-    width,
-    color: backgroundColor,
-    model_viewer_attributes,
-    model_viewer_version: modelViewerVersion,
-  } = argv;
-  const inputPath = `http://localhost:${modelPort}/${path.basename(input)}`;
-  const [outputPath, format, formatExtension] = parseOutputPathAndFormat(output, image_format);
-  const defaultBackgroundColor =
-    format === "image/jpeg" ? colors.white : colors.transparent;
-  let modelViewerArgs: { [key: string]: string } = undefined;
+	const {
+		input,
+		output,
+		image_format,
+		image_quality: quality,
+		timeout,
+		height,
+		width,
+		color: backgroundColor,
+		model_viewer_attributes,
+		model_viewer_version: modelViewerVersion,
+	} = argv;
+	const inputPath = input;
+	const [outputPath, format, formatExtension] = parseOutputPathAndFormat(
+		output,
+		image_format
+	);
+	const defaultBackgroundColor =
+		format === "image/jpeg" ? colors.white : colors.transparent;
+	let modelViewerArgs: { [key: string]: string } = undefined;
 
-  if (model_viewer_attributes) {
-    modelViewerArgs = {};
+	if (model_viewer_attributes) {
+		modelViewerArgs = {};
 
-    const params = new URLSearchParams(model_viewer_attributes);
-    params.forEach((value, key) => {
-      modelViewerArgs[key] = value;
-    });
-  }
+		const params = new URLSearchParams(model_viewer_attributes);
+		params.forEach((value, key) => {
+			modelViewerArgs[key] = value;
+		});
+	}
 
-  return {
-    backgroundColor: backgroundColor || defaultBackgroundColor,
-    quality,
-    timeout,
-    height,
-    width,
-    debug,
-    inputPath,
-    outputPath,
-    format,
-    formatExtension,
-    modelViewerArgs,
-    modelViewerVersion,
-  };
+	return {
+		backgroundColor: backgroundColor || defaultBackgroundColor,
+		quality,
+		timeout,
+		height,
+		width,
+		debug,
+		inputPath,
+		outputPath,
+		format,
+		formatExtension,
+		modelViewerArgs,
+		modelViewerVersion,
+	};
 }
